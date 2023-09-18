1d120ee383f37e6b73034a4b31fc11db1339f7d9
Muchamad Faiz <109614025+muchamadfaiz@users.noreply.github.com>
2023-09-18 23:14:50 +0700

    adding data

---
+++ src/data.ts
@@ -0,0 +1,44 @@
+export enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
+
+export const data: Data = {
+  report: [
+    {
+      id: 'uuid1',
+      source: 'salary',
+      amount: 8000,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid2',
+      source: 'salary',
+      amount: 9000,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid3',
+      source: 'fuel',
+      amount: 8000,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+  ],
+};
+
+interface Data {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: ReportType;
+  }[];
+}
